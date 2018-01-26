d8e3ce5027cd13ee535261fa73a0e6e1cb074aa0
Darkhitori <34727429+Darkhitori@users.noreply.github.com>
2018-01-26 11:37:07 -0500

    Update vMCI_OnReceiveAttack.cs

--- vMeleeCombatInput/vMCI_OnReceiveAttack.cs
+++ vMeleeCombatInput/vMCI_OnReceiveAttack.cs
@@ -110,7 +110,7 @@ namespace HutongGames.PlayMaker.Actions
 			dam.recoil_id = recoil_id.Value;
 			dam.reaction_id = reaction_id.Value;
 			dam.attackName = attackName.Value;
-			var vDam = dam;
+			var vDam = new vDamage(dam);
 			
 			theScript.OnReceiveAttack(vDam, vMelee);
 			if(sendEvent == null)
